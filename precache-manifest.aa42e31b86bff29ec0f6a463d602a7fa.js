ef0aa78a0f16163cefe6d905a7ae1656c1453943
Ayush <ayushdabhi31@gmail.com>
2022-07-09 15:23:59 +0530

    Updates

---
+++ precache-manifest.aa42e31b86bff29ec0f6a463d602a7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7e3b1fd59b73c380ade287a0966487",
+    "url": "/ExpenseManager/index.html"
+  },
+  {
+    "revision": "af3880e2446d3750f31d",
+    "url": "/ExpenseManager/static/css/main.ef3b4491.chunk.css"
+  },
+  {
+    "revision": "e331f5219e1c5016724d",
+    "url": "/ExpenseManager/static/js/2.04f970d3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ExpenseManager/static/js/2.04f970d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3880e2446d3750f31d",
+    "url": "/ExpenseManager/static/js/main.3f927432.chunk.js"
+  },
+  {
+    "revision": "a123fe82cfce126f4f4f",
+    "url": "/ExpenseManager/static/js/runtime-main.049292f2.js"
+  }
+]);
